df90e5e6ca79af2c1235d4c20eba58e81e64e962
krdurgesh610 <kr.durgesh610@gmail.com>
2024-06-12 18:04:11 +0530

    Arrays Solutions

--- Arrays/src/org/dugu/LargestElement.java
+++ Arrays/src/org/dugu/LargestElement.java
@@ -20,8 +20,23 @@ public class LargestElement {
 		return -1;
 	}
 
+//	=========================> Efficient Solution <=================
+
+	static int largestIndex(int[] arr) {
+		int n = arr.length;
+		int max = 0;
+		for (int i = 0; i < n; i++) {
+			if (arr[i] > arr[max]) {
+				max = i;
+			}
+		}
+		return max;
+	}
+
 	public static void main(String[] args) {
 		int[] arr = { 10, 3, 9, 15, 2, 55 };
 		System.out.println(largestElementIndex(arr));
+		System.out.println("===================");
+		System.out.println(largestIndex(arr));
 	}
 }
