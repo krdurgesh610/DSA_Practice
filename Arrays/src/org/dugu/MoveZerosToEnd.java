4ef449289b9c5aa1ed39262149460d4d1cad7676
krdurgesh610 <kr.durgesh610@gmail.com>
2024-06-14 23:20:47 +0530

    Solutions of Arrays

---
+++ Arrays/src/org/dugu/MoveZerosToEnd.java
@@ -0,0 +1,33 @@
+package org.dugu;
+
+public class MoveZerosToEnd {
+
+	static void moveZerosToEnd(int arr[]) {
+		int n = arr.length;
+
+		int count = 0;
+
+		for (int i = 0; i < n; i++)
+			if (arr[i] != 0)
+				arr[count++] = arr[i];
+
+		for (int i = count; i < n; i++)
+			arr[i] = 0;
+	}
+
+	static void printArray(int[] arr) {
+		int n = arr.length;
+
+		for (int i = 0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+		int arr[] = { 0, 1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9 };
+		printArray(arr);
+		moveZerosToEnd(arr);
+		printArray(arr);
+	}
+}
